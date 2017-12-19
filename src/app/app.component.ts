0e4a1909e5ca25360a2776d98287583881d54670
Leandro Ricardo <leandro.j.c.ricardo@gmail.com>
2017-12-19 23:31:19 +0000

    Changed the root page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { AddNewPage } from '../pages/add-new/add-new';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = AddNewPage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
